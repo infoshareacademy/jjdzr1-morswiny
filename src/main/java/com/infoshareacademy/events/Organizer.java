aac95ab790074e1a482d7e947c2c6a34ef1ac9ca
SudolAgata <agatasudol@o2.pl>
2020-06-17 21:23:15 +0200

    Adding constructor & toString to Organizer class

--- src/main/java/com/infoshareacademy/events/Organizer.java
+++ src/main/java/com/infoshareacademy/events/Organizer.java
@@ -4,6 +4,18 @@ public class Organizer {
     private String id;
     private String designation;
 
+    public Organizer(String id, String designation) {
+        this.id = id;
+        this.designation = designation;
+    }
+
+    @Override
+    public String toString() {
+        return "Organizer " +
+                "id = " + id +
+                ", designation = " + designation ;
+    }
+
     public String getId() {
         return id;
     }
