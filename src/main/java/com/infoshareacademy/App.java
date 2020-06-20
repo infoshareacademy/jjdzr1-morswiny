9446d1510aed69b7aaf50a53730dd07ad113be15
Tomek Miecielica <tomek.miecielica@gmail.com>
2020-06-20 12:02:52 +0200

    Added logic to search Events by a random String of min. 3 characters

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -4,6 +4,9 @@ import com.infoshareacademy.events.Event;
 import com.infoshareacademy.events.EventRepository;
 import com.infoshareacademy.navigation.Menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -19,15 +22,17 @@ public class App
 
         EventRepository asd = new EventRepository();
         //Event event = new Event();
-        //event.dateTimeFormatter("2020-06-16T23:00:00+0200");
+//        event.dateTimeFormatter("2020-06-16T23:00:00+0200");
         asd.arrayToSet();
-
-        asd.showAllEvents();
+//
+//        asd.showAllEvents();
 //        asd.showSingleEvent(71890);
 
 //        System.out.println(asd.getEventSet().toString());
 
-
+        String userInput = asd.getUserQuery();
+        List<Event> list = new ArrayList<>();
+        list =asd.searchByString(userInput);
         
     }
 }
